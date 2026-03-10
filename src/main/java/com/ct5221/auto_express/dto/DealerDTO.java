b9110735b5a136fbea5c6acbca61ca19c2edf52d
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-09 21:45:40 -0500

    Added the new attributes to the DTO classes.

--- src/main/java/com/ct5221/auto_express/dto/DealerDTO.java
+++ src/main/java/com/ct5221/auto_express/dto/DealerDTO.java
@@ -5,16 +5,22 @@ import java.util.List;
 public class DealerDTO {
     private Long id;
     private String username;
+    private String firstName;
+    private String lastName;
     private String email;
+    private Integer age;
     private String phone;
     private String password;
     private List<Long> vehicleIds;
 
     public DealerDTO(){}
 
-    public DealerDTO(String username, String email, String phone, String password){
+    public DealerDTO(String username, String firstName, String lastName, String email, Integer age, String phone, String password){
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
+        this.age = age;
         this.phone = phone;
         this.password = password;
     }
@@ -25,8 +31,17 @@ public class DealerDTO {
     public String getUsername(){return username;}
     public void setUsername(String username) { this.username = username;}
 
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
 
     public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
--- src/main/java/com/ct5221/auto_express/dto/VehicleDTO.java
+++ src/main/java/com/ct5221/auto_express/dto/VehicleDTO.java
@@ -5,17 +5,19 @@ public class VehicleDTO {
     private Long id;
     private String make;
     private String model;
-    private int year;
+    private Integer year;
     private Double price;
+    private Double mileage;
     private Long dealerId;
 
     public VehicleDTO() {}
 
-    public VehicleDTO(String make, String model, int year, Double price, Long dealerId) {
+    public VehicleDTO(String make, String model, Integer year, Double price, Double mileage, Long dealerId) {
         this.make = make;
         this.model = model;
         this.year = year;
         this.price = price;
+        this.mileage = mileage;
         this.dealerId = dealerId;
     }
 
@@ -28,11 +30,14 @@ public class VehicleDTO {
     public String getModel() { return model; }
     public void setModel(String model) { this.model = model; }
 
-    public int getYear() { return year; }
+    public Integer getYear() { return year; }
     public void setYear(int year) { this.year = year; }
 
     public Double getPrice() { return price; }
     public void setPrice(Double price) { this.price = price; }
+
+    public Double getMileage() { return mileage; }
+    public void setMileage(Double mileage) {this.mileage = mileage;}
 
     public Long getDealerId() { return dealerId; }
     public void setDealerId(Long dealerId) { this.dealerId = dealerId;}
