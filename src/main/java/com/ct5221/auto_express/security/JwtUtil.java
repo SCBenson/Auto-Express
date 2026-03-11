7d93a45ea17c7f3e48954168ae067a79a760459f
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-11 18:16:03 -0500

    feat: implement JWT utility with JJWT 0.12.6 API. - Add JwtUtil class for JWT token generation and validation. - Use JJWT 0.12.6 MacAlgorithm API for HS256 signing. - Token extracdtion methods for username and expiration. - Add 1 hr token expiration time. - Include token validation against username and expiration. Future: Move secret key to enviornment variables for production.

--- src/main/java/com/ct5221/auto_express/security/JwtUtil.java
+++ src/main/java/com/ct5221/auto_express/security/JwtUtil.java
@@ -1,4 +1,64 @@
 package com.ct5221.auto_express.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class JwtUtil {
+    //Store this securely for production.
+    private final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject){
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, String username){
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
 }
