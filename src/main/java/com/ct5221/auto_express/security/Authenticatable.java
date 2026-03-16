b435b201800febef8406d7f6a1b21ec57af472db
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-16 16:12:38 -0500

    interface that contains the structure of the getters for id, email, password and role for security authentication for dealers and users for login.

--- src/main/java/com/ct5221/auto_express/security/Authenticatable.java
+++ src/main/java/com/ct5221/auto_express/security/Authenticatable.java
@@ -1,4 +1,8 @@
 package com.ct5221.auto_express.security;
 
-public class Authenticatable {
+public interface Authenticatable {
+    Long getId();
+    String getEmail();
+    String getPassword();
+    String getRole();
 }
