73b9aef3630e86821bbe541f3171bc56b13fecdc
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-17 17:47:00 -0500

    commented out the Regex pattern validation at the entity level, as the password is checked for this pattern with AuthService.

--- src/main/java/com/ct5221/auto_express/domain/Dealer.java
+++ src/main/java/com/ct5221/auto_express/domain/Dealer.java
@@ -31,8 +31,8 @@ public class Dealer implements Authenticatable {
     @Size(min = 10, max=10, message = "Phone number must be 10 digits")
     private String phone;
     @Size(min = 8, message = "Password must be at least 8 characters long")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+    //@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            //message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
     private String password;
 
     private String dealershipName;
