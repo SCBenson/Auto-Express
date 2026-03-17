edd252b3679884aa031931d720f82c3135e26f52
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-03-17 17:43:27 -0500

    implemented registration HTTP endpoints for users and dealers along with a login endpoint.

--- src/main/java/com/ct5221/auto_express/controller/AuthController.java
+++ src/main/java/com/ct5221/auto_express/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.ct5221.auto_express.controller;
 
+import com.ct5221.auto_express.dto.DealerDTO;
 import com.ct5221.auto_express.dto.LoginRequest;
 import com.ct5221.auto_express.dto.LoginResponse;
+import com.ct5221.auto_express.dto.UserDTO;
 import com.ct5221.auto_express.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,26 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
+        try {
+            UserDTO registeredUser = authService.registerUser(userDTO);
+            return ResponseEntity.ok(registeredUser);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/register/dealer")
+    public ResponseEntity<?> registerDealer(@RequestBody DealerDTO dealerDTO){
+        try {
+           DealerDTO registeredDealer = authService.registerDealer(dealerDTO);
+           return ResponseEntity.ok(registeredDealer);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
